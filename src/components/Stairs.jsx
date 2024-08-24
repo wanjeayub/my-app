abcf0a8754ad1e1e3e0e85a93b3992cbcfb3bd8d
Ayub Wanje <ayub.wanje@gmail.com>
2024-08-24 17:16:05 +0300

    initial commit with nav bar and all pages created

---
+++ src/components/Stairs.jsx
@@ -0,0 +1,47 @@
+import { motion } from "framer-motion";
+
+// variants
+const stairAnimation = {
+  initial: {
+    top: "0%",
+  },
+  animate: {
+    top: "100%",
+  },
+  exit: {
+    top: ["100%", "0%"],
+  },
+};
+
+// calculate the reverse index for staggered delay
+const reverseIndex = (index) => {
+  const totalSteps = 6;
+  return totalSteps - index - 1;
+};
+
+const Stairs = () => {
+  return (
+    <>
+      {/* render 6 motion divs  */}
+      {[...Array(6)].map((_, index) => {
+        return (
+          <motion.div
+            key={index}
+            variants={stairAnimation}
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            transition={{
+              duration: 0.4,
+              ease: "easeInOut",
+              delay: reverseIndex(index) * 0.1,
+            }}
+            className="h-full w-full bg-white relative"
+          />
+        );
+      })}
+    </>
+  );
+};
+
+export default Stairs;
