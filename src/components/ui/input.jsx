408aef7db2ada5c87072fd62a1e476e583231a97
Ayub Wanje <ayub.wanje@gmail.com>
2024-09-05 10:33:59 +0300

    Update props for input element to allow value and name props

--- src/components/ui/input.jsx
+++ src/components/ui/input.jsx
@@ -6,6 +6,8 @@ const Input = React.forwardRef(({ id, className, type, ...props }, ref) => {
   return (
     <input
       id={id}
+      value={value}
+      name={name}
       type={type}
       className={cn(
         "flex h-[48px] rounded-md border border-white/10 focus:border-accent font-light bg-primary px-4 py-5 text-base placeholder:text-white/60 outline-none",
