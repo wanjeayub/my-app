6f22f9d35a3ac19e44081ec0a94f6c9b137496fe
Ayub Wanje <ayub.wanje@gmail.com>
2024-10-14 12:01:33 +0300

    change public key and debug error - not sending email

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -47,9 +47,9 @@ const Contact = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const serviceID = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID;
-    const templateID = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID;
-    const userID = process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY;
+    const serviceID = process.env.NEXT_EMAILJS_SERVICE_ID;
+    const templateID = process.env.NEXT_EMAILJS_TEMPLATE_ID;
+    const publicKEY = process.env.NEXT_EMAILJS_PUBLIC_KEY;
 
     try {
       const emailParams = {
@@ -62,7 +62,7 @@ const Contact = () => {
         serviceID,
         templateID,
         emailParams,
-        userID
+        publicKEY
       );
 
       if (res.status === 200) {
